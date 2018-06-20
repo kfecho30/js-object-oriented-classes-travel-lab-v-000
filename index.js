c7330b5bca5bbb9d3e476f4ce595e4938b68e672
Kelsey Fecho <kfecho30@gmail.com>
2018-06-20 19:35:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
 class Driver {
   constructor(name, date){
     this.name = name;
@@ -15,7 +17,11 @@ class Route {
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
   }
+
+  aveToInt(ave) {
+    return eastWest.indexOf(ave)
+  }
   blocksTravelled(){
-    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
   }
 }
