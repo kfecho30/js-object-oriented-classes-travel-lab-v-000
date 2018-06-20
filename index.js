83eed1e5c8965e428d17132baa35210bf6cc739d
Kelsey Fecho <kfecho30@gmail.com>
2018-06-20 19:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,4 +26,11 @@ class Route {
     let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
     return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
   }
+  estimatedTime(peak){
+    if(peak){
+      return blocksTravelled() /2
+    } else {
+      return blocksTavelled() / 3
+    }
+  }
 }
