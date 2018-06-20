6bf22d6acb309ceac8ada1ed056331a40eec698b
Kelsey Fecho <kfecho30@gmail.com>
2018-06-20 19:29:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 class Driver {
   constructor(name, date){
     this.name = name;
-    this.date = date;
   }
 
-  startDate(this.date) {
-    Date(date)
+  startDate() {
   }
   yearsExperienceFromBeginningOf(){
 
