e877fcf78923cd354eb426d98e5fdadb81075bcf
Kelsey Fecho <kfecho30@gmail.com>
2018-06-20 19:26:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ class Driver {
 
   }
 }
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+}
