0b0abb62a51cb5e837f344dc2fde1e1476427434
Kelsey Fecho <kfecho30@gmail.com>
2018-06-20 19:27:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Driver {
   }
 
   startDate() {
-    
+
   }
   yearsExperienceFromBeginningOf(){
 
@@ -15,5 +15,8 @@ class Route {
   constructor(beginningLocation, endingLocation){
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
+  }
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
   }
 }
